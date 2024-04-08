074427161fb2cb78962a4f2241f3508e4ba9624b
nkmlworld <nirakaar09@gmail.com>
2024-04-08 10:42:30 +0530

    stage 01

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
-with open("README.md","r", encoding="utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="src",
-    version="0.0.1",
+    version="0.0.3",
     author="nkmlworld",
-    description="A small pkg for dvc ml pipeline",
+    description="A small package for dvc ml pipeline demo",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nkmlworld/simple-dvc-project",
     author_email="nirakaar09@gmail.com",
-    package_dir={"":"src"},
-    packages=find_packages(where="src"),
+    # package_dir={"": "src"},
+    # packages=find_packages(where="src"),
+    packages=["src"],
     license="GNU",
     python_requires=">=3.6",
     install_requires=[
